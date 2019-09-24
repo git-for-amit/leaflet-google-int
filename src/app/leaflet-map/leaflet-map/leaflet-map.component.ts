465d933139d94ecdca947ca206eab17a3e6b649c
Amit Dube <adube@wm.com>
2019-09-24 12:41:09 +0530

    fix for loading google map by removing leaflet import and removed commented code

--- src/app/leaflet-map/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map/leaflet-map.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import * as L from 'leaflet';
 
 @Component({
   selector: 'app-leaflet-map',
@@ -22,13 +21,6 @@ export class LeafletMapComponent implements OnInit, AfterViewInit {
       type: 'roadmap'
     }).addTo(this.leafletMap);
     
-    /* L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 18,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox.streets'
-      }).addTo(this.leafletMap); */
   }
 
 }
